e2415d80053cec508a29b30978ed4e1436ef4906
Ulvi <ulvi199517@gmail.com>
2020-06-28 22:51:52 -0400

    robofriend app created with react

---
+++ src/components/SeachBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SeachBox = ({searchfield, searchChange}) => {
+    return (
+        <div className="pa2">
+            <input 
+                type="seach" 
+                placeholder="seach robots" 
+                className="pa3 ba b--green bg-lightest-blue"
+                onChange={searchChange}
+            />
+        </div>
+        
+    );
+}
+export default SeachBox;
